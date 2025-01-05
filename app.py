0d8b560ffb1f21366e26918a45054919c74f6a88
HeuryPy <heuryneves@gmail.com>
2025-01-05 13:00:32 -0300

    adicionado botão deslizante e o botão de programa

--- app.py
+++ app.py
@@ -6,4 +6,13 @@ import streamlit as st
 
 st.header('Jogando uma moeda')
 
+'''Adicionando o controle deslizante e o botão ao programa:
+
+'''
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    
 st.write('Ainda não é um aplicativo funcional. Em construção.')
